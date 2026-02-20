de07152d244bd3d92eb488fae33a629f718a4e91
felipesarian <felipe.sarian@gmail.com>
2026-02-20 12:54:28 -0300

    0.2

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -11,10 +11,12 @@ int multiplyNumbers(int x, int y) {
 int main(void) {
     int a=5,b=2; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
+    printf("%d\n",result);
 
     int arr[NUM_ELEMENTS];
     for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
+        printf("%d\n",arr[i]);
     }
 
     return 0;
